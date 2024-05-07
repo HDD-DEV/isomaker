2eb4da2516cd8bdc4bc8e72474d3ba9347901cfd
HDD-DEV <160027919+HDD-DEV@users.noreply.github.com>
2024-05-07 06:10:40 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "updating repositories"
+sudo apt update
+echo "installing dependencies"
+sudo apt install grub grub-common mtools xorriso wget curl git -y
+sleep 2
+echo "Downloading isomaker"
+cd ~/
+mkdir isomaker
+cd isomaker
+git clone https://github.com/HDD-DEV/isomaker
+cd /usr/bin
+sudo cp ~/isomaker/isomaker /usr/bin/
+sudo chmod +x /usr/bin/isomaker
+sleep 3
+echo "YAY!Isomaker was installed with no errors"
+sleep 2
+exit
